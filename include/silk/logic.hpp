49bfec03bbaa668b32af4c6fa4dcfa702ad53277
Adam P. Goucher <goucher@statslab.cam.ac.uk>
2024-05-12 22:01:37 +0100

    Function to sum the cells in the punctured Moore neighbourhood from two bitplanes

--- include/silk/logic.hpp
+++ include/silk/logic.hpp
@@ -53,4 +53,73 @@ _DI_ bool stableprop(uint32_t &ad0, uint32_t &ad1, uint32_t &ad2, uint32_t &al2,
     return false;
 }
 
+
+/**
+ * Returns the mod-16 sum of the 16 booleans obtained by taking the
+ * values of a and b on each of the 8 neighbours. For our application
+ * (computing lower and upper bounds on the change, versus the stable
+ * state, of the number of live cells in the neighbourhood) the change
+ * is bounded in [-6, +8] so we only need to know the bound mod 16.
+ */
+_DI_ uint4 sum16(uint32_t a, uint32_t b) {
+
+    // 4 warp shuffles:
+    uint32_t al = kc::shift_plane<true,  1>(a);
+    uint32_t ar = kc::shift_plane<true, -1>(a);
+    uint32_t bl = kc::shift_plane<true,  1>(b);
+    uint32_t br = kc::shift_plane<true, -1>(b);
+
+    uint32_t o2 = apply_maj3(al, ar, bl);
+    uint32_t o1 = apply_xor3(al, ar, bl);
+
+    // the sum of the upper and lower cells is in {o2, o1, br}
+
+    uint32_t p2 = apply_maj3(o1, a, b);
+    uint32_t p1 = apply_xor3(o1, a, b);
+
+    // the sum of this column of 3 is in {o2, p2, p1, br}
+
+    // 8 shifts:
+    uint32_t o2u = kc::shift_plane<false,  1>(o2);
+    uint32_t p2u = kc::shift_plane<false,  1>(p2);
+    uint32_t p1u = kc::shift_plane<false,  1>(p1);
+    uint32_t bru = kc::shift_plane<false,  1>(br);
+    uint32_t o2d = kc::shift_plane<false, -1>(o2);
+    uint32_t p2d = kc::shift_plane<false, -1>(p2);
+    uint32_t p1d = kc::shift_plane<false, -1>(p1);
+    uint32_t brd = kc::shift_plane<false, -1>(br);
+
+    uint32_t q2 = apply_maj3(bru, brd, br);
+    uint32_t q1 = apply_xor3(bru, brd, br);
+    uint32_t r2 = apply_maj3(p1u, p1d, o1);
+    uint32_t r1 = apply_xor3(p1u, p1d, o1);
+
+    uint4 res;
+
+    // the sum of the neighbourhood is in {o2u, p2u, o2d, p2d, o2, q2, r2, q1, r1}
+    // compute lowest bit and carry:
+    res.x = q1 ^ r1;
+    uint32_t s2 = q1 & r1;
+    uint32_t t4 = apply_maj3(o2u, o2d, o2);
+    uint32_t t2 = apply_xor3(o2u, o2d, o2);
+    uint32_t u4 = apply_maj3(p2u, p2d, q2);
+    uint32_t u2 = apply_xor3(p2u, p2d, q2);
+    uint32_t v4 = apply_maj3(r2, s2, t2);
+    uint32_t v2 = apply_xor3(r2, s2, t2);
+
+    // the sum of the neighbourhood is in {t4, u4, v4, u2, v2, res.x}
+    // compute next bit and carry:
+    res.y = u2 ^ v2;
+    uint32_t w4 = u2 & v2;
+    uint32_t x8 = apply_maj3(t4, u4, v4);
+    uint32_t x4 = apply_xor3(t4, u4, v4);
+
+    // the sum of the neighbourhood is in {x8, x4, w4, res.y, res.x}
+    // compute upper two bits:
+    res.z = x4 ^ w4;
+    res.w = x8 ^ (x4 & w4);
+
+    return res;
+}
+
 }
---
+++ test/gpu/unit/test_sum16.cu
@@ -0,0 +1,70 @@
+#include <silk/logic.hpp>
+#include <cpads/random/prng.hpp>
+#include <gtest/gtest.h>
+
+__global__ void sum16_kernel(uint4 *x) {
+
+    uint4 a = x[blockIdx.x * 32 + threadIdx.x];
+    uint4 b = kc::sum16(a.x, a.y);
+    x[blockIdx.x * 32 + threadIdx.x] = b;
+
+}
+
+void check_sum16(uint32_t* input, uint32_t* output) {
+
+    for (int y = 1; y < 31; y++) {
+        for (int x = 1; x < 31; x++) {
+
+            int expected = 0;
+            int actual = 0;
+
+            for (int oy = y - 1; oy <= y + 1; oy++) {
+                for (int ox = x - 1; ox <= x + 1; ox++) {
+                    if ((ox == x) && (oy == y)) { continue; }
+                    expected += ((input[oy * 4] >> ox) & 1);
+                    expected += ((input[oy * 4 + 1] >> ox) & 1);
+                }
+            }
+
+            actual +=  (output[y * 4    ] >> x) & 1;
+            actual += ((output[y * 4 + 1] >> x) & 1) * 2;
+            actual += ((output[y * 4 + 2] >> x) & 1) * 4;
+            actual += ((output[y * 4 + 3] >> x) & 1) * 8;
+
+            EXPECT_EQ(actual, expected & 15);
+        }
+    }
+}
+
+TEST(Sum16, Sum16) {
+
+    int N = 1000;
+
+    uint32_t* input;
+    uint32_t* output;
+    uint4* device_buffer;
+
+    cudaMallocHost((void**) &input, N << 9);
+    cudaMallocHost((void**) &output, N << 9);
+    cudaMalloc((void**) &device_buffer, N << 9);
+
+    hh::PRNG pcg(1, 2, 3);
+
+    for (int i = 0; i < N * 128; i++) {
+        input[i] = pcg.generate();
+    }
+
+    cudaMemcpy(device_buffer, input, N << 9, cudaMemcpyHostToDevice);
+    sum16_kernel<<<N, 32>>>(device_buffer);
+    cudaMemcpy(output, device_buffer, N << 9, cudaMemcpyDeviceToHost);
+
+    cudaFree(device_buffer);
+
+    for (int i = 0; i < N; i++) {
+        check_sum16(input + i * 128, output + i * 128);
+    }
+
+    cudaFreeHost(input);
+    cudaFreeHost(output);
+
+}
