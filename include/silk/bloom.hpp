276eb2c169b322468f26af295f637d0a90338e6a
Adam P. Goucher <goucher@statslab.cam.ac.uk>
2024-06-11 14:22:31 +0100

    Bloom filter

---
+++ include/silk/bloom.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <cpads/core.hpp>
+
+namespace kc {
+
+_DI_ void inplace_vertical_circulant(uint64_t &x, int i, int j) {
+    uint32_t y = ((uint32_t) x);
+    y = hh::shuffle_32(y, threadIdx.x + i) ^ hh::shuffle_32(y, threadIdx.x + j);
+    x ^= y;
+    x = hh::fibmix(x);
+}
+
+_DI_ uint64_t warp_hash(uint64_t input) {
+    uint64_t x = hh::fibmix(input + 6700417 * (threadIdx.x & 31));
+    inplace_vertical_circulant(x, 22, 27);
+    inplace_vertical_circulant(x, 21, 6);
+    inplace_vertical_circulant(x, 18, 9);
+    return x;
+}
+
+_DI_ uint32_t partial_shuffle(uint32_t x, uint32_t mask, int lane) {
+    uint32_t y = hh::shuffle_xor_32(x, lane);
+    return (x & mask) | (y &~ mask);
+}
+
+_DI_ uint32_t random_rotate(uint32_t x, uint32_t y) {
+    return (x << (y & 31)) | (x >> ((-y) & 31));
+}
+
+/**
+ * Produces a 1024-bit value (32 bits per thread) where exactly 32 of the
+ * 1024 bits are set.
+ */
+_DI_ uint32_t sparse_random(uint32_t input) {
+    uint32_t x = (1u << (input & 31));
+    x = partial_shuffle(x, hh::ballot_32(input & 32), 1);
+    x = random_rotate(x, input >> 10);
+    x = partial_shuffle(x, hh::ballot_32(input & 64), 2);
+    x = random_rotate(x, input >> 15);
+    x = partial_shuffle(x, hh::ballot_32(input & 128), 4);
+    x = random_rotate(x, input >> 20);
+    x = partial_shuffle(x, hh::ballot_32(input & 256), 8);
+    x = random_rotate(x, input >> 25);
+    x = partial_shuffle(x, hh::ballot_32(input & 512), 16);
+    x = random_rotate(x, input);
+    return x;
+}
+
+}
---
+++ test/gpu/unit/test_bloom.cu
@@ -0,0 +1,45 @@
+#include <silk/bloom.hpp>
+#include <cpads/random/prng.hpp>
+#include <gtest/gtest.h>
+
+__global__ void bloom_test_kernel(uint32_t *res) {
+
+    uint32_t x = res[blockIdx.x * blockDim.x + threadIdx.x];
+    x = kc::sparse_random(x);
+    res[blockIdx.x * blockDim.x + threadIdx.x] = x;
+
+}
+
+TEST(Bloom, SparseRandom) {
+
+    int N = 100000;
+
+    uint32_t* a_h;
+    uint32_t* a_d;
+
+    cudaMallocHost((void**) &a_h, 128 * N);
+    cudaMalloc((void**) &a_d, 128 * N);
+
+    hh::PRNG pcg(1, 2, 3);
+
+    for (int i = 0; i < 32*N; i++) {
+        a_h[i] = pcg.generate();
+    }
+
+    cudaMemcpy(a_d, a_h, 128 * N, cudaMemcpyHostToDevice);
+    bloom_test_kernel<<<(N >> 5), 1024>>>(a_d);
+    cudaMemcpy(a_h, a_d, 128 * N, cudaMemcpyDeviceToHost);
+
+    cudaFree(a_d);
+
+    for (int i = 0; i < N; i++) {
+        int p = 0;
+        for (int j = 0; j < 32; j++) {
+            p += hh::popc32(a_h[i*32+j]);
+        }
+        EXPECT_EQ(p, 32);
+    }
+
+    cudaFreeHost(a_h);
+
+}
