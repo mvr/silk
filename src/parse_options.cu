f8c544dfa1e58334e47086540aa3875200832708
Adam P. Goucher <goucher@statslab.cam.ac.uk>
2024-06-15 17:04:41 +0100

    Print help message when -h or --help is specified

--- src/parse_options.cu
+++ src/parse_options.cu
@@ -10,30 +10,38 @@ int main(int argc, char* argv[]) {
 
     std::string silk_filename = argv[0];
     std::string nnue_filename = silk_filename + "_nnue.dat";
-    std::cerr << "Info: Silk invoked as " << silk_filename << std::endl;
 
     // define argument parser
     cxxopts::Options options("silk", "A CUDA drifter searcher");
 
     options.add_options()
 
-        // positional arguments (obligatory)
-        ("input_filename", "LifeHistory RLE specifying the problem", cxxopts::value<std::string>())
-        ("max_active_width", "maximum width of active region", cxxopts::value<int>())
-        ("max_active_height", "maximum height of active region", cxxopts::value<int>())
-        ("max_active_cells", "maximum number of active cells", cxxopts::value<int>())
+    // positional arguments (obligatory)
+    ("input_filename", "LifeHistory RLE specifying the problem", cxxopts::value<std::string>())
+    ("max_active_width", "maximum width of active region", cxxopts::value<int>())
+    ("max_active_height", "maximum height of active region", cxxopts::value<int>())
+    ("max_active_cells", "maximum number of active cells", cxxopts::value<int>())
 
-        // optional arguments
-        ("cadicals", "number of CaDiCaL threads to stabilise results", cxxopts::value<int>()->default_value("8"))
-        ("p,period", "minimum period of oscillators to report", cxxopts::value<int>()->default_value("999999999"))
-        ("d,dataset", "filename of dataset to output", cxxopts::value<std::string>()->default_value(""))
-        ("s,min-stable", "minimum unclean catalyst stable time before report", cxxopts::value<int>()->default_value("999999999"))
-    ;
+    // optional arguments
+    ("cadicals", "number of CaDiCaL threads to stabilise results", cxxopts::value<int>()->default_value("8"))
+    ("p,period", "minimum period of oscillators to report", cxxopts::value<int>()->default_value("999999999"))
+    ("d,dataset", "filename of dataset to output", cxxopts::value<std::string>()->default_value(""))
+    ("s,min-stable", "minimum unclean catalyst stable time before report", cxxopts::value<int>()->default_value("999999999"))
+
+    // help
+    ("h,help", "Print usage");
 
     options.parse_positional({"input_filename", "max_active_width", "max_active_height", "max_active_cells"});
+    options.positional_help("path/to/input.rle max_active_width max_active_height max_active_cells");
 
     // apply argument parser to cmdline args
     auto result = options.parse(argc, argv);
+
+    if (result.count("help")) {
+        // print help message and exit
+        std::cerr << options.help() << std::endl;
+        return 0;
+    }
 
     // extract positional arguments
     std::string input_filename = result["input_filename"].as<std::string>();
@@ -46,6 +54,8 @@ int main(int argc, char* argv[]) {
     int min_report_period = result["period"].as<int>();
     std::string dataset_filename = result["dataset"].as<std::string>();
     int min_stable = result["min-stable"].as<int>();
+
+    std::cerr << "Info: Silk invoked as " << silk_filename << std::endl;
 
     // run program
     int return_code = silk_main(
