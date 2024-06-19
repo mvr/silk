b94fe9385172c0876928ae12018937a5e0466f19
Adam P. Goucher <goucher@statslab.cam.ac.uk>
2024-06-19 13:15:46 +0100

    Strengthen random bit permutation

--- include/silk/bloom.hpp
+++ include/silk/bloom.hpp
@@ -27,15 +27,7 @@ _DI_ uint32_t random_rotate(uint32_t x, uint32_t y) {
     return (x << (y & 31)) | (x >> ((-y) & 31));
 }
 
-/**
- * Produces a 1024-bit value (32 bits per thread) where exactly k of the
- * 1024 bits are set.
- */
-_DI_ uint32_t sparse_random(uint32_t input, int k) {
-
-    uint32_t x = 0;
-
-    if ((threadIdx.x & 31) < k) { x = (1u << (input & 31)); }
+_DI_ void warp_bit_permute(uint32_t &x, uint32_t input) {
     x = partial_shuffle(x, hh::ballot_32(input & 32), 16);
     x = random_rotate(x, input >> 10);
     x = partial_shuffle(x, hh::ballot_32(input & 64), 8);
@@ -46,6 +38,18 @@ _DI_ uint32_t sparse_random(uint32_t input, int k) {
     x = random_rotate(x, input >> 25);
     x = partial_shuffle(x, hh::ballot_32(input & 512), 1);
     x = random_rotate(x, input);
+}
+
+/**
+ * Produces a 1024-bit value (32 bits per thread) where exactly k of the
+ * 1024 bits are set (where k can be between 0 and 32, inclusive). This
+ * is a deterministic function of the 2048-bit hash (64 bits per thread).
+ */
+_DI_ uint32_t sparse_random(uint64_t hash, int k) {
+    uint32_t x = 0;
+    if ((threadIdx.x & 31) < k) { x = (1u << (hash & 31)); }
+    warp_bit_permute(x, ((uint32_t) (hash >> 32)));
+    warp_bit_permute(x, ((uint32_t) hash));
     return x;
 }
 
