45ffba39eecb2149c17e94f7164b99b749d8d2e0
Adam P. Goucher <goucher@statslab.cam.ac.uk>
2024-07-16 00:52:36 +0100

    Unit test that exposes Clang bug

---
+++ test/gpu/unit/test_clang_bug.cu
@@ -0,0 +1,81 @@
+#include <silk/torus.hpp>
+#include <cpads/random/prng.hpp>
+#include <gtest/gtest.h>
+
+
+/**
+ * This kernel is incorrect on Clang but correct on nvcc.
+ */
+__global__ void buggy_kernel(const uint32_t *src, uint32_t *dst) {
+
+    int i = blockIdx.x & 63;
+    int j = blockIdx.x >> 6;
+
+    uint32_t a = src[blockIdx.x * 32 + threadIdx.x];
+    uint32_t b = 0;
+    uint32_t c = 0;
+    uint32_t d = 0;
+
+    kc::shift_torus_inplace(a, b, c, d, i, j);
+
+    dst[blockIdx.x * 32 + threadIdx.x] = a;
+}
+
+
+/**
+ * This kernel is correct on both Clang and nvcc.
+ */
+__global__ void reference_kernel(const uint32_t *src, uint32_t *dst) {
+
+    int i = blockIdx.x & 63;
+    int j = blockIdx.x >> 6;
+
+    uint32_t a = src[blockIdx.x * 32 + threadIdx.x];
+
+    kc::shift_plane_inplace(a, i, j);
+
+    dst[blockIdx.x * 32 + threadIdx.x] = a;
+}
+
+
+TEST(Clang, Bug) {
+
+    constexpr int n = 128 * 64 * 64;
+    uint32_t* d_a;
+    uint32_t* d_b;
+    uint32_t* d_c;
+    uint32_t* h_a;
+    uint32_t* h_b;
+    uint32_t* h_c;
+
+    cudaMalloc((void**) &d_a, n);
+    cudaMalloc((void**) &d_b, n);
+    cudaMalloc((void**) &d_c, n);
+    cudaMallocHost((void**) &h_a, n);
+    cudaMallocHost((void**) &h_b, n);
+    cudaMallocHost((void**) &h_c, n);
+
+    hh::PRNG pcg(1, 2, 3);
+
+    for (int i = 0; i < (n >> 2); i++) {
+        // generate some random data
+        h_a[i] = pcg.generate();
+    }
+
+    cudaMemcpy(d_a, h_a, n, cudaMemcpyHostToDevice);
+    reference_kernel<<<4096, 32>>>(d_a, d_b);
+    buggy_kernel<<<4096, 32>>>(d_a, d_c);
+    cudaMemcpy(h_b, d_b, n, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_c, d_c, n, cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < (n >> 2); i++) {
+        EXPECT_EQ(h_b[i], h_c[i]);
+    }
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    cudaFreeHost(h_a);
+    cudaFreeHost(h_b);
+    cudaFreeHost(h_c);
+}
